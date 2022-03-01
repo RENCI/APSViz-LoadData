77be070cae6d992f668abede19fcee9dbc25bcc4
Lisa Stillwell <lisa@renci.org>
2022-03-01 09:30:02 -0500

    added cog test

--- test/add_coverage_store.py
+++ test/add_coverage_store.py
@@ -10,6 +10,48 @@ class FileReader:
     def read_callback(self, size):
         return self.fp.read(size)
 
+def create_cog_coveragestore(instance_id, path, username, password, service_url, workspace):
+    print("Creating COG coveragestore")
+    content_type = 'image/tiff'
+    overwrite = False
+    file_name = os.path.basename(path)
+    print(file_name)
+    f = file_name.split(".")
+    if len(f) > 0:
+        file_name = f[0]
+    else:
+        print(f"Ilegal file name: {file_name}")
+        sys.exit(0)
+    lyr_name = f"{instance_id}_{file_name}"
+    print(lyr_name)
+    file_type = f"mbtiles?configure=first&coverageName={lyr_name}"
+
+    try:
+        file_size = os.path.getsize(path)
+        c = pycurl.Curl()
+        file_name = lyr_name
+
+        c.setopt(pycurl.USERPWD, username + ':' + password)
+        c.setopt(c.URL, '{0}/rest/workspaces/{1}/coveragestores/{2}/file.{3}'.format(
+            service_url, workspace, file_name, file_type))
+        c.setopt(pycurl.HTTPHEADER, [
+                 "Content-type:{}".format(content_type)])
+        c.setopt(pycurl.READFUNCTION, FileReader(
+            open(path, 'rb')).read_callback)
+        c.setopt(pycurl.INFILESIZE, file_size)
+        if overwrite:
+            c.setopt(pycurl.PUT, 1)
+        else:
+            c.setopt(pycurl.POST, 1)
+        c.setopt(pycurl.UPLOAD, 1)
+        c.setopt(pycurl.VERBOSE, 1)
+        c.perform()
+        certinfo = c.getinfo_raw(pycurl.INFO_CERTINFO)
+        print(certinfo)
+        c.close()
+    except Exception as e:
+        print('Error: {}'.format(e))
+
 def main(args):
 
     workspace = "ADCIRC_2021"
@@ -45,6 +87,7 @@ def main(args):
     password = args.password.strip()
     print(password)
 
+    create_cog_coveragestore(instance_id, path, username, password, service_url, workspace)
 
     content_type='application/vnd.sqlite3'
     overwrite=False
